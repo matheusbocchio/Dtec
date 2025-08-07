9c00c2288a8d2c0fc84e801ab9563a56ba1aec28
matheusbocchio <matheusbocchio@gmail.com>
2025-08-07 15:01:12 -0300

    FABRICA DE FUNÇÃO

--- aulas/back/js/script2.js
+++ aulas/back/js/script2.js
@@ -3,8 +3,31 @@ const pessoa = {
     sobrenome: "xavier",
     idade: 50
 }
-
+//ACESSAR O ATRIBUTO 
 console.log(pessoa.nome)
+console.log(pessoa.sobrenome)
+console.log(pessoa.idade)
+
+// FUNÇÃO FABRICA
+function criarPessoas(nome, sobrenome, idade) {
+    return{
+        nome,
+        sobrenome,
+        idade
+    };
+};
+
+const pessoa2 = criarPessoas("maria", "silva", 25)
+const pessoa3 = criarPessoas("joão", "roberto", 20)
+console.log(pessoa2.nome)
+console.log(pessoa3.idade)
+
+
+
+
+
+
+
 
 
 
