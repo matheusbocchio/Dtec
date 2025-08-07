c9a8e04338fb946c0d272cad521a65821ae9afe2
matheusbocchio <matheusbocchio@gmail.com>
2025-08-07 14:20:21 -0300

    como funciona função

---
+++ aulas/back/js/script.js
@@ -0,0 +1,33 @@
+/*//<---- FUNÇÕES DECLARADAS --->
+
+//Criação da função
+function soma (a,b){
+    return a + b
+}
+
+function saudacao(){
+    console.log("boa tarde");
+}
+
+
+
+//UTILIZAÇÃO DA FUNÇÃO
+console.log(soma(3,5))
+const resultado = soma(10,8)
+console.log(resultado)
+console.log(soma(2,9))
+console.log(saudacao)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
