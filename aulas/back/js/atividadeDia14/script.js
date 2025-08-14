be198167f50b1e036312e3a163dd8dd849b35c54
matheusbocchio <matheusbocchio@gmail.com>
2025-08-14 16:40:03 -0300

    atividade de revição

---
+++ aulas/back/js/atividadeDia14/script.js
@@ -0,0 +1,83 @@
+
+// 1°
+console.log("matheus de oliveira bocchio")
+console.log("17 anos")
+
+// 2°/ 3°
+let nome = "matheus"
+let idade = "17 anos"
+let saudacao = "olá bom dia"
+console.log(nome)
+console.log(idade)
+console.log(saudacao)
+console.log(saudacao + " eu me chamo, " + nome + " e eu tenho, " + idade)
+//4°
+let num1 = 20
+let num2 = "20"
+console.log(num1 + num1);
+console.log(num2 + num2);
+console.log(num1 + num2);
+
+//5°
+let usuario = prompt("fale o seu nome ");
+alert("seu nome é " + usuario);
+
+//6°
+let confirmacao = confirm("você aceita receber as notificações?")
+if (confirmacao) {
+    alert("agora você vai receber as novidades")
+} else {
+    alert("ok, você não recebera as notificações")
+}
+
+//7°
+let numero1 = Number(prompt("fale um numero"))
+let numero2 = Number(prompt("fale outro numero"))
+alert(numero1 + numero2)
+
+//8°
+let nomeUsuario = prompt("qual é o seu nome?")
+let confirmacaoIdade = confirm("você tem mais de 18 anos?")
+if (confirmacaoIdade) {
+    alert(nomeUsuario + "e você tem acesso ao site")
+} else {
+    alert(nomeUsuario + "e você não tem acesso ao site")
+}
+
+//9°
+let nome1 = "matheus"
+console.log(nome1)
+let nome2 = "joão"
+console.log(nome2)
+
+//10°
+let nomeCliente = "matheus"
+console.log(nomeCliente.toUpperCase())
+console.log(nomeCliente.toLowerCase())
+
+//11°
+let nomecliente = "matheus"
+let NOMECLIENTE = "joão"
+console.log(nomecliente)
+console.log(NOMECLIENTE)
+
+//12° 
+let x = "y"
+let y = "x"
+
+let letra1 = x
+let letra2 = y
+
+//13° 
+const nu1 = 13;
+const nu2 = 22;
+const resultado = nu1 + nu2
+console.log(resultado)
+
+//14° 
+
+
+
+
+
+
